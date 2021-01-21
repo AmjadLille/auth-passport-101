79a533f4cf4036b50056229b226317cc748568d5
Loic <loic.brassart@gmail.com>
2021-01-21 17:12:19 +0100

    Add code about user registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const { db, jwt_rounds, jwt_secret } = require("../conf");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
-router.post("/signup", (req, res) => {
-    const msg =
-        "Here, we must work on user registration";
-    res.status(200).send(msg);
+router.post("/signup", async (req, res) => {
+    try {
+        const formData = req.body;
+        formData.password = bcrypt.hashSync(formData.password, jwt_rounds);
+        const [sqlRes] = await db.query(`INSERT INTO user SET ?`, formData);
+        delete formData.password;
+        formData.id = sqlRes.insertId;
+        const token = jwt.sign(formData, jwt_secret);
+        res.status(201).json(token);
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e);
+    }
 })
+
 router.post("/login", (req, res) => {
     const msg =
         "Here, we must see if our user already exists";
