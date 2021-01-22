687563db14cda2786778555d223dda088a98b605
Loic <loic.brassart@gmail.com>
2021-01-22 18:02:33 +0100

    Add strategies ans complete auth system

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,29 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 const { db, jwt_rounds, jwt_secret } = require("../conf");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+require("../passport-strategies");
 
 router.post("/signup", async (req, res) => {
-    try {
-        const formData = req.body;
-        formData.password = bcrypt.hashSync(formData.password, jwt_rounds);
-        const [sqlRes] = await db.query(`INSERT INTO user SET ?`, formData);
-        delete formData.password;
-        formData.id = sqlRes.insertId;
-        const token = jwt.sign(formData, jwt_secret);
-        res.status(201).json(token);
-    } catch (e) {
-        console.log(e);
-        res.status(500).json(e);
-    }
-})
+  try {
+    const formData = req.body;
+    formData.password = bcrypt.hashSync(formData.password, jwt_rounds);
+    const [sqlRes] = await db.query(`INSERT INTO user SET ?`, formData);
+    delete formData.password;
+    formData.id = sqlRes.insertId;
+    const token = jwt.sign(formData, jwt_secret);
+    res.status(201).json(token);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
 
-router.post("/login", (req, res) => {
-    const msg =
-        "Here, we must see if our user already exists";
-    res.status(200).send(msg);
-})
-
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  const token = jwt.sign(req.user, jwt_secret);
+  res.status(200).json(token);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { backPort } = require("./conf");
+const passport = require("passport");
 
 const app = express();
 app.use(express.json());
+app.use(passport.initialize());
 
 /* --------------------------------------------------------------------- Routes */
 
@@ -11,11 +13,11 @@ app.use("/", require("./routes/misc"));
 
 /* --------------------------------------------------------------------- 404 and server launch */
 app.use((req, res) => {
-    const msg = `Page not found: ${req.url}`;
-    console.warn(msg);
-    res.status(404).send(msg);
+  const msg = `Page not found: ${req.url}`;
+  console.warn(msg);
+  res.status(404).send(msg);
 });
 
 app.listen(backPort, () => {
-    console.log(`API root available at: http://localhost:${backPort}/`);
+  console.log(`API root available at: http://localhost:${backPort}/`);
 });
